1815510c430706a89b219ca72063027f925b4024
testeryup <enzo.edwin.zzz@gmail.com>
2024-09-15 22:19:48 +0700

    add user controller and more user service

---
+++ src/services/userService.js
@@ -0,0 +1,89 @@
+import { where } from "sequelize"
+import db from "../models/index"
+import { resolveInclude } from "ejs"
+import bcrypt from "bcryptjs"
+let handleUserLogin = (email, password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let userData = {};
+            let isExit = await checkUserEmail(email);
+
+            let user = await db.User.findOne({
+                where: {email: email},
+                attributes: ['email', 'roleId', 'password'],
+                raw: true
+            })
+
+            if (isExit) {
+                let check = await bcrypt.compareSync(password, user.password);
+                if (check) {
+                    userData.errCode = 0;
+                    userData.errMessage = 'ok';
+
+                    delete user.password;
+                    
+                    userData.user = user;
+                }
+                else{
+                    userData.errCode = 3;
+                    userData.errMessage = 'Wrong password!';
+                }
+            }
+            else{
+                userData.errCode = 1;
+                userData.errMessage = `User does not exist!`;
+            }
+            resolve(userData);
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+let compareUserPassword = (password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            userData = {};
+            let isExit = await checkUserEmail(email);
+            if (isExit) {
+                let check = await bcrypt.compareSync(password, user.password);
+                if (check) {
+                    userData.errCode = 0;
+                    userData.errMessage = 'ok';
+                    userData.user = user;
+                }
+                else{
+                    userData.errCode = 3;
+                    userData.errMessage = 'Wrong password!';
+                }
+            }
+            else{
+                userData.errCode = 2;
+                userData.errMessage = `User does not exist!`;
+            }
+            resolve(userData);
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+let checkUserEmail = (userEmail) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: {email: userEmail}
+            })
+            if (user) {
+                resolve(true);
+            }
+            else{
+                resolve(false);
+            }
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+module.exports = {
+    handleUserLogin
+}
