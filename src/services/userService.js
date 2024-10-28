6128e6781ea36d20a05f7f8fc29952a9abe63b3b
testeryup <enzo.edwin.zzz@gmail.com>
2024-10-28 21:14:44 +0700

    change handle data function

--- src/services/userService.js
+++ src/services/userService.js
@@ -13,7 +13,7 @@ let handleUserLogin = (email, password) => {
 
             let user = await db.User.findOne({
                 where: {email: email},
-                attributes: ['email', 'roleId', 'password'],
+                attributes: ['email', 'roleId', 'password', 'firstName', 'lastName'],
                 raw: true
             })
 
