2f37561f8e2b2157d1414d8aff3f98429cbb47cc
testeryup <enzo.edwin.zzz@gmail.com>
2024-11-13 10:47:39 +0700

    add save infor doctor

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -1,7 +1,7 @@
 import doctorService from '../services/doctorService'
 let getTopDoctorHome = async (req, res) => {
     let limit = req.query.limit;
-    if(!limit) limit = 10;
+    if (!limit) limit = 10;
     try {
         let response = await doctorService.getTopDoctorHome(+limit);
         return res.status(200).json(response);
@@ -13,7 +13,30 @@ let getTopDoctorHome = async (req, res) => {
         })
     }
 }
-
+let getAllDoctors = async (req, res) => {
+    try {
+        let doctors = await doctorService.getAllDoctors()
+        return res.status(200).json(doctors);
+    } catch (error) {
+        console.log(error)
+        return res.status(200).json({
+            errCode: 1,
+            errMessage: 'Error from server!'
+        })
+    }
+}
+let postInforDoctor = async (req, res) => {
+    try {
+        let response = await doctorService.saveDetailInforDoctor(req.body);
+        return res.status(200).json(response);
+    } catch (error) {
+        console.log(error)
+        return res.status(200).json({
+            errCode: 1,
+            errMessage: 'Error from server!'
+        })
+    }
+}
 module.exports = {
-    getTopDoctorHome
+    getTopDoctorHome, getAllDoctors, postInforDoctor
 }
--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,17 +1,18 @@
+// import { where } from 'sequelize'
 import db from '../models/index'
 let getTopDoctorHome = (limit) => {
     return new Promise(async (resolve, reject) => {
         try {
             let users = await db.User.findAll({
-                where: {roleId: 'R2'},
+                where: { roleId: 'R2' },
                 limit: limit,
                 order: [["createdAt", "DESC"]],
                 attributes: {
-                    exclude: ['password', ]
+                    exclude: ['password',]
                 },
                 include: [
-                    {model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi']},
-                    {model: db.Allcode, as: 'genderData', attributes: ['valueEn', 'valueVi']}
+                    { model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi'] },
+                    { model: db.Allcode, as: 'genderData', attributes: ['valueEn', 'valueVi'] }
                 ],
                 raw: true,
                 nest: true
@@ -27,6 +28,52 @@ let getTopDoctorHome = (limit) => {
     })
 }
 
+let getAllDoctors = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let doctors = await db.User.findAll({
+                where: {roleId: 'R2'},
+                attributes: {
+                    exclude: ['password', 'image']
+                }
+            })
+            resolve({
+                errCode: 0,
+                data: doctors
+            })
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+let saveDetailInforDoctor = (inputData) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if(!inputData.id || !inputData.contentHTML || !inputData.contentMarkdown) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing parameters!'
+                })
+            }
+            else{
+                await db.Markdown.create({
+                    contentHTML: inputData.contentHTML,
+                    contentMarkdown: inputData.contentMarkdown,
+                    description: inputData.description,
+                    doctorId: inputData.id
+                })
+
+                resolve({
+                    errCode: 0,
+                    errMessage: 'Doctor details saved successfully!'
+                })
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    })
+}
 module.exports = {
-    getTopDoctorHome
+    getTopDoctorHome, getAllDoctors, saveDetailInforDoctor
 }
