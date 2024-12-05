3dd3605823ec6a320227c6e152d104c972036cc7
testeryup <enzo.edwin.zzz@gmail.com>
2024-12-05 11:22:33 +0700

    create new api for get profile doctor by id

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -87,6 +87,18 @@ let getExtraDoctorInforById = async (req, res) => {
         })
     }
 }
+
+let getProfileDoctorById = async (req, res) => {
+    try {
+        let infor = await doctorService.getProfileDoctorById(req.query.doctorId);
+        return res.status(200).json(infor);
+    } catch (error) {
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: "Error from server!"
+        })
+    }
+}
 module.exports = {
-    getTopDoctorHome, getAllDoctors, postInforDoctor, getDetailDoctorById, bulkCreateSchedule, getScheduleByDate, getExtraDoctorInforById
+    getTopDoctorHome, getAllDoctors, postInforDoctor, getDetailDoctorById, bulkCreateSchedule, getScheduleByDate, getExtraDoctorInforById, getProfileDoctorById
 }
