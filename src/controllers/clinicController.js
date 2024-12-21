33fd7c9d8c5e97f99e5eaae5b42c5563e90cfa91
testeryup <enzo.edwin.zzz@gmail.com>
2024-12-21 16:54:46 +0700

    add get detail clinic by id, get all doctor from clinic

--- src/controllers/clinicController.js
+++ src/controllers/clinicController.js
@@ -12,6 +12,32 @@ let createClinic = async (req, res) => {
         })
     }
 }
+
+let getAllClinic = async (req, res) => {
+    try {
+        let response = await clinicService.getAllClinic();
+        return res.status(200).json(response);
+    } catch (error) {
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: "Error from server!",
+            error
+        })
+    }
+}
+
+let getDetailClinicById = async (req, res) => {
+    try {
+        let response = await clinicService.getDetailClinicById(req.query.id);
+        return res.status(200).json(response);
+    } catch (error) {
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: "Error from server!",
+            error
+        })
+    }
+}
 module.exports = {
-    createClinic
+    createClinic, getAllClinic, getDetailClinicById
 }
--- src/services/clinicService.js
+++ src/services/clinicService.js
@@ -2,33 +2,93 @@ import db from "../models";
 
 let createClinic = (data) => {
     return new Promise(async (resolve, reject) => {
-            // console.log("check service:", data);
-            try {
-                if (!data.name || !data.address || !data.imageBase64 || !data.descriptionHTML || !data.descriptionMarkdown) {
-                    resolve({
-                        errCode: 1,
-                        errMessage: "Missing input parameters!"
-                    });
-                }
-                else {
-                    await db.Clinic.create({
-                        name: data.name,
-                        address: data.address,
-                        image: data.imageBase64,
-                        descriptionHTML: data.descriptionHTML,
-                        descriptionMarkdown: data.descriptionMarkdown
-                    });
-                    resolve({
-                        errCode: 0,
-                        errMessage: "Create clinic succeed!"
-                    });
-                }
-            } catch (error) {
-                console.log(error);
-                reject(error);
+        // console.log("check service:", data);
+        try {
+            if (!data.name || !data.address || !data.imageBase64 || !data.descriptionHTML || !data.descriptionMarkdown) {
+                resolve({
+                    errCode: 1,
+                    errMessage: "Missing input parameters!"
+                });
             }
-        });
+            else {
+                await db.Clinic.create({
+                    name: data.name,
+                    address: data.address,
+                    image: data.imageBase64,
+                    descriptionHTML: data.descriptionHTML,
+                    descriptionMarkdown: data.descriptionMarkdown
+                });
+                resolve({
+                    errCode: 0,
+                    errMessage: "Create clinic succeed!"
+                });
+            }
+        } catch (error) {
+            console.log(error);
+            reject(error);
+        }
+    });
+}
+
+let getAllClinic = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let data = await db.Clinic.findAll({
+
+            });
+            if (data?.length > 0) {
+                data = data.map((item) => {
+                    item.image = item.image.toString();
+                    return item;
+                });
+            }
+            resolve({
+                errCode: 0,
+                data
+            })
+        } catch (error) {
+            console.log(error);
+            reject(error);
+        }
+    });
+}
+let getDetailClinicById = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!id) {
+                resolve({
+                    errCode: 1,
+                    errMessage: "Missing input parameters!"
+                });
+            }
+            else {
+                let data = await db.Clinic.findOne({
+                    where: {id: id},
+                    attributes: ['descriptionHTML', 'name', 'address']
+                });
+                
+                if(data){
+                    let doctorClinic = [];
+                    doctorClinic = await db.Doctor_Infor.findAll({
+                        where: {clinicId: id},
+                        attributes: ['doctorId', 'provinceId']
+                    })
+                    data.doctorClinic = doctorClinic;
+                }
+                else{
+                    data = {};
+                }
+                resolve({
+                    errCode: 0, 
+                    data
+                });
+            }
+        } catch (error) {
+            console.log(error);
+            reject(error);
+        }
+    });
 }
 module.exports = {
-    createClinic
+    createClinic, getAllClinic, getDetailClinicById
 }
